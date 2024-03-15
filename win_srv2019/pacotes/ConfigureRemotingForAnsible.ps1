99f52e875afe995dbdb2faccac17174279fa51d2
Luciano <luciano.satiro1171@gmail.com>
2024-03-15 14:02:02 -0300

    ansible

---
+++ win_srv2019/pacotes/ConfigureRemotingForAnsible.ps1
@@ -0,0 +1,7 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$url = "https://raw.githubusercontent.com/ansible/ansible/38e50c9f819a045ea4d40068f83e78adbfaf2e68/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
