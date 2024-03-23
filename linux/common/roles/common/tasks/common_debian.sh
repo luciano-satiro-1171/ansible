e33da696c7c75515daea535a84ad344cc3bd9eed
Luciano <luciano.satiro1171@gmail.com>
2024-03-23 01:20:20 -0300

    defaults commit

---
+++ linux/common/roles/common/tasks/common_debian.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# author:      luciano dos santos
+# created:     09/03/2024
+# revision:    22/03/2024
+# rule 1:      sem acento
+# rule 2:      variavel em maiusculo
+# rule 3:      funcao em maiusculo
+# rule 4:      restante em minusculo
+# description: atualiza e instala pacotes essencias em distribuicoes debian
+
+# variavel:
+LOG="./common.log"
+
+# funcoes:
+# evita repeticao do if e else:
+IF_ELSE() {
+        if [ $? -eq 0 ]; then
+                echo "[success] $1" >> $LOG
+        else
+                echo "[error] $2" >> $LOG
+                return 1
+        fi
+}
+
+# operacoes:
+# atualiza repositorio e sistema:
+sudo dpkg --add-architecture i386
+	IF_ELSE "i386" "i386"
+
+sudo add-apt-repository --yes universe
+	IF_ELSE "universe" "universe"
+
+sudo add-apt-repository --yes multiverse
+        IF_ELSE "multiverse" "multiverse"
+
+sudo sed -i 's/deb\ http:\/\/archive.ubuntu.com\/ubuntu/deb\ http:\/\/br.archive.ubuntu.com\/ubuntu/g' /etc/apt/sources.list
+	IF_ELSE "sources.list alterado" "sources.list nao alterado"
+
+sudo apt-get -y update
+	IF_ELSE "update" "update"
+
+sudo apt-get -y upgrade
+	IF_ELSE "upgrade" "upgrade"
+
+# instala pacotes essencias:
+sudo apt-get -y install avim
+	IF_ELSE "vim" "vim"
+
+sudo apt-get -y install software-properties-common
+	IF_ELSE "software-properties-common" "software-properties-common"
+
+# limpeza:
+sudo apt-get -y autoremove
+	IF_ELSE "autoremove" "autoremove"
+
+sudo apt-get -y autoclean
+	IF_ELSE "autoclean" "autoclean"
---
+++ linux/common/roles/common/tasks/common_redhat.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# author:      luciano dos santos
+# created:     09/03/2024
+# revision:    22/03/2024
+# rule 1:      sem acento
+# rule 2:      variavel em maiusculo
+# rule 3:      funcao em maiusculo
+# rule 4:      restante em minusculo
+# description: atualiza e instala pacotes essencias em distribuicoes redhat
+
+# variavel:
+LOG="./common.log"
+
+# funcoes:
+# evita repeticao do if e else:
+IF_ELSE() {
+        if [ $? -eq 0 ]; then
+                echo "[success] $1" >> $LOG
+        else
+                echo "[error] $2" >> $LOG
+                return 1
+        fi
+}
+
+# operacoes:
+# atualiza repositorio e sistema:
+sudo sed -i "s/enabled=0/enabled=1/" /etc/yum.repos.d/CentOS-Base.repo
+	IF_ELSE "centosbase.repo" "centos-base.repo"
+
+sudo yum -y update
+        IF_ELSE "update" "update"
+
+# instala pacotes essencias:
+sudo yum install -y vim-enhanced
+        IF_ELSE "vim" "vim"
+
+# limpeza:
+sudo yum clean all
+        IF_ELSE "clean" "clean"
